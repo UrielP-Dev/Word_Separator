04098ab58742359ff9209801d7cf4f07f0d90bf3
Uriel-PP <kaattoo9@gmail.com>
2023-09-08 21:17:34 -0600

    Initial-commit

---
+++ scripts/Read.py
@@ -0,0 +1,18 @@
+class ReadFile:
+    def open_file(self):
+        archivo = open('Files/File.txt', 'r')
+        self.text = archivo.read()
+        archivo.close()
+
+    def check_text(self):
+        with open('Files/File_out.txt', 'w') as Text_out:
+            character = input('Enter character: ')
+            for caracter in self.text:
+                if caracter == ' ':
+                    Text_out.write('\n')
+                elif caracter == character:
+                    Text_out.write(character)
+                    Text_out.write('\n')    
+                        
+                else:
+                    Text_out.write(caracter)
---
+++ scripts/main.py
@@ -0,0 +1,5 @@
+from Read import ReadFile
+
+lector = ReadFile()
+lector.open_file()
+lector.check_text()
