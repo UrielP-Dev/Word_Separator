fe9b07e930f2509dbe4b1d57b88ae81bff0238af
Uriel-PP <kaattoo9@gmail.com>
2023-10-16 00:11:54 -0600

    Commit

--- scripts/Read.py
+++ scripts/Read.py
@@ -1,19 +1,41 @@
 class ReadFile:
     def open_file(self):
-        archivo = open('Files/File.txt', 'r')
-        self.text = archivo.read()
-        archivo.close()
-
+        with open('Files/File.txt', 'r') as archivo:
+            self.text = archivo.read()
+        
     def check_text(self):
         with open('Files/File_out.txt', 'w') as Text_out:
-            character = input('Enter characters separated by spaces: ').split()
+            previous_character = ''  # Almacena el caracter anterior para verificar líneas en blanco
+            special_chars = {'@', '"'}
+            within_special = False  # Indica si estamos dentro de un carácter especial
+            current_special_char = None  # Almacena el carácter especial actual
             for caracter in self.text:
-                if caracter == ' ':
-                    Text_out.write('\n')
-                elif caracter in character:
-                    Text_out.write('\n')
+                if caracter in special_chars:
+                    if within_special:
+                        within_special = False
+                        if caracter == current_special_char:
+                            current_special_char = None
+                    else:
+                        within_special = True
+                        current_special_char = caracter
+
+                if caracter in {':', ';', '+', '*', '/', '<', '>', '[', ']', '!', '(', ')', ' '}:
+                    if previous_character != '\n' and not within_special:  # Evitar escribir líneas en blanco consecutivas
+                        Text_out.write('\n')
                     Text_out.write(caracter)
-                    Text_out.write('\n')    
-                        
                 else:
                     Text_out.write(caracter)
+                previous_character = caracter
+
+
+    
+    def remove_blank_lines_from_output(self):
+        # Leer el archivo File_out.txt y eliminar líneas en blanco
+        with open('Files/File_out.txt', 'r') as file:
+            lines = file.readlines()
+        
+        lines = [line for line in lines if line.strip()]  # Filtrar líneas en blanco
+
+        # Sobrescribir File_out.txt con las líneas restantes
+        with open('Files/File_out.txt', 'w') as file:
+            file.writelines(lines)       
--- scripts/main.py
+++ scripts/main.py
@@ -3,3 +3,4 @@ from Read import ReadFile
 lector = ReadFile()
 lector.open_file()
 lector.check_text()
+lector.remove_blank_lines_from_output()
